8266f64e27d3590d8cab5355ee917b8fca26ce65
Clara Smyth <clarasmyth.work@gmail.com>
2022-07-05 19:26:58 +0100

    Moved creating dom elements to seperate module

---
+++ src/modules/create-dom-elements.js
@@ -0,0 +1,56 @@
+const createDiv = (className) => {
+    const div = document.createElement('div');
+    div.classList.add(className);
+    return div;
+}
+
+const createH2 = (className) => {
+    const h2 = document.createElement('h2');
+    h2.classList.add(className);
+    return h2;
+}
+
+const createPara = (className) => {
+    const para = document.createElement('p');
+    para.classList.add(className);
+    return para;
+}
+
+const createBtn = (className) => {
+    const btn = document.createElement('button');
+    btn.classList.add(className);
+    return btn;
+}
+
+const createInput = (className, inputType) => {
+    const input = document.createElement('input');
+    input.classList.add(className);
+    input.setAttribute('type', inputType);
+    return input;
+}
+
+const createTodoCard = (todo) => {
+    const container = createDiv('todo-card');
+    const checkbox = createInput('todo-card-input', 'checkbox');
+    const title = createPara('todo-card-title');
+    const date = createPara('todo-card-date');
+    const editBtn = createBtn('todo-card-edit');
+    const deleteBtn = createBtn('todo-card-delete');
+
+    checkbox.checked = todo.checked;
+    title.innerText = todo.title;
+    date.innerText = todo.date;
+
+    container.append(checkbox, title, date, editBtn, deleteBtn);
+    
+    return container;
+}
+
+export {
+    createDiv,
+    createH2,
+    createPara,
+    createBtn,
+    createInput,
+    createTodoCard,
+};
