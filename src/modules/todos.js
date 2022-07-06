df23cbebda0a2c5719923d1c262c4bde02305f8f
Clara Smyth <clarasmyth.work@gmail.com>
2022-07-06 02:27:58 +0100

    Added a factory for creating new todos

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -5,4 +5,16 @@ const todos = [{
     date: 'dd-mm-year',
 }];
 
-export { todos };
+ const  todoFactory = (title, date) => {
+    const type = 'general';
+    const checked = false;
+    return { type, checked, title, date }
+ }
+
+ const createTodo = (title, date) => {
+    const newTodo = todoFactory(title, date);
+    todos.push(newTodo);
+    console.log(todos);
+ };
+
+export { todos, createTodo };
