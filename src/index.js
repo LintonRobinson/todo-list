eb80efb26e12c41486fc3cfb59f07ae032e0cbb6
Clara Smyth <clarasmyth.work@gmail.com>
2022-07-09 00:07:16 +0100

    Added projects file and button to create projects

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import { todos } from './modules/todos.js';
 import { buildPage } from './modules/general.js';
+import { openProjectModal } from './modules/modal.js';
 
 const main = document.querySelector('.main');
+const navBtn = document.querySelector('.header-nav-btn');
 
 main.append(buildPage());
+
+navBtn.addEventListener('click', () => {
+    openProjectModal();
+})
+
+
+
+
 
 const test = todos.filter(checkType)
 
